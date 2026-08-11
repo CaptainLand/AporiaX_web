757053ca17ff6183ab71b795bcf4a7ed0f28675e
SeaLand1129 <18270616289@163.com>
2026-08-12 00:42:21 +0800

    Load final AporiaX blue accent

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./styles.css";
 import "./aporiax-theme.css";
 import "./product-hero.css";
+import "./blue-accent.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
